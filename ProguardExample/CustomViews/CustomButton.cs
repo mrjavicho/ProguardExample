92d10f585d50b6347cc5f36cf3e0efba5bfbe015
Javier <javier.huamanchumo@tetrasoft-studios.es>
2016-10-08 11:33:34 +0200

    Initial commit

---
+++ ProguardExample/CustomViews/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+
+namespace ProguardExample
+{
+	public class CustomButton : Button
+	{
+		protected CustomButton(IntPtr javaReference, JniHandleOwnership transfer) :base(javaReference, transfer)
+		{
+		}
+
+		public CustomButton(Context context) : base (context){ }
+
+		public CustomButton(Context context, IAttributeSet attrs) : base (context, attrs) { }
+
+		public CustomButton(Context context, IAttributeSet attrs, int defStyleAttr) : base (context, attrs, defStyleAttr) { }
+
+		public CustomButton(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base (context, attrs, defStyleAttr, defStyleRes){ }
+	}
+}
